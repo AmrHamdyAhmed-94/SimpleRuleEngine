6d0848b1eda7a6c6f9cc3285af7f284543e462aa
Amr Hamdy <amrhamdyahmed94@gmail.com>
2026-07-26 16:39:02 +0300

    initial commit

---
+++ src/test/java/com/simpleRuleEngine/SimpleRuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleRuleEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
